5eda3f48ad45d797754941be5ace59f80bb0d8c6
Théo Faucher <theo.fchr1@gmail.com>
2018-11-05 22:09:53 +0100

    hmm

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 
-
+let test3 = 'Ferme';
 let app = express();
 let port = process.env.PORT | 8080;
 
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
     fs.readFile('fichier.txt', (err, data) => {
         // res.render('index', {kietrentre:data});
         fs.readFile('ouvertferme.txt', (err, data2) => {
-            res.render('index', {kietrentre:data, test:data2}); 
+            res.render('index', {kietrentre:data, test:test3}); 
         });
     });
 });
@@ -27,34 +27,20 @@ app.get('/post/fauchert', (req, res) => {
     let my_date = new Date();
     let my_hour = my_date.getHours();
     let my_minute = my_date.getMinutes();
-    if(my_minute < 10)
-    {
-
-        my_minute = "0"+my_minute
-
-
-    }
     let my_good_time = my_hour +1 + "h" + "0" + my_minute;
     fs.writeFile('fichier.txt',  " Théo Faucher à " + my_good_time , (err) => {});
-    fs.writeFile('ouvertferme.txt', 'Ouvert', (err) => {});
-    res.end(my_good_time);
+    res.render('index', {test:"Ouvert"}); 
+       res.end(my_good_time);
 })
 
 app.get('/post/lejeunem', (req, res) => {
     let my_date = new Date();
     let my_hour = my_date.getHours();
     let my_minute = my_date.getMinutes();
-    if(my_minute < 10)
-    {
-
-        my_minute = "0"+my_minute
-
-
-    }
-    let my_good_time = my_hour +1 + "h" + my_minute;
-    fs.writeFile('fichier.txt', " Maxime Le Jeune à " + my_good_time, (err) => {});
-    fs.writeFile('ouvertferme.txt', 'Ouvert', (err) => {});
-    res.end(my_good_time);
+    let my_good_time = my_hour +1 + "h" + "0" + my_minute;
+        fs.writeFile('fichier.txt', " Maxime Le Jeune à " + my_good_time, (err) => {});
+        res.render('index', {test:"Ouvert"}); 
+        res.end(my_good_time);
 });
 
 
