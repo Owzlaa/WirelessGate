b731bca4dbe09a815fe57456f424d1506b48bfdd
Théo Faucher <theo.fchr1@gmail.com>
2018-11-07 22:36:16 +0100

    Optimisation, history of person enter

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
-      fs = require('fs');
-      bodyParser = require('body-parser');
-      ejs = require('ejs'); 
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const ejs = require('ejs'); 
 
-let nomprenom;
-    app = express();
-    port = process.env.PORT | 8080;
+let app = express();
+let port = process.env.PORT | 8080;
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -27,40 +26,42 @@ app.get('/', (req, res) => {
 
 
 app.get('/post/fauchert', (req, res) => {
+    let my_date = new Date();
+    let my_hour = my_date.getHours();
+    let my_minute = my_date.getMinutes();
+    if(my_minute < 10)
+    {
 
-    nomprenom = 'Théo Faucher';
-    fonction( req, res);
+        my_minute = "0" + my_minute
 
+
+    }
+    let my_good_time = my_hour +1 + "h" + my_minute;
+    fs.writeFile('ouvertferme.txt', 'Ouvert', (err) => {});
+    fs.copyFileSync('fichier2nd.txt', 'fichier3eme.txt', (err) => {});
+    fs.copyFileSync('fichier1er.txt', 'fichier2nd.txt', (err) => {});
+    fs.writeFile('fichier1er.txt',  "Théo Faucher à " + my_good_time , (err) => {});
+    res.end(my_good_time);
 })
 
 app.get('/post/lejeunem', (req, res) => {
-
-    nomprenom = 'Maxime Le Jeune';
-    fonction( req, res);
-
-})
-
-function fonction( req, res)
-{
-
     let my_date = new Date();
+    let my_hour = my_date.getHours();
     let my_minute = my_date.getMinutes();
     if(my_minute <= 10)
     {
 
-        my_minute = "0" + my_minute;
+        my_minute = "0" + my_minute          
 
     }
-    let my_good_time =my_date.getHours() +1 + "h" + my_minute;
+    let my_good_time = my_hour +1 + "h" + my_minute;
     fs.writeFile('ouvertferme.txt', 'Ouvert', (err) => {});
     fs.copyFileSync('fichier2nd.txt', 'fichier3eme.txt', (err) => {});
     fs.copyFileSync('fichier1er.txt', 'fichier2nd.txt', (err) => {});
-    fs.writeFile('fichier1er.txt',  nomprenom + " à " + my_good_time , (err) => {});
+    fs.writeFile('fichier1er.txt',  "Maxime Le Jeune à " + my_good_time , (err) => {});
     res.end(my_good_time);
+})
 
-
-
-}
 
 app.get('/post/ferme', (req, res) => {
     fs.writeFile('ouvertferme.txt', 'Fermé', (err) => {
