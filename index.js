99685b7839231fb7226179693c5a758b341ff6cb
Théo Faucher <33026623+Owzlaa@users.noreply.github.com>
2018-11-23 16:16:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ function fonction( req, res)    //Fontion qui permet d'éxécuter à plusieurs e
 
 app.get('/post/ferme', (req, res) => {
     fs.writeFile('files/files/ouvertferme.txt', 'Fermé', (err) => {
-          res.end();
+          res.end("ferme");
     })
 });
 
